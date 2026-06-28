700f766d72ca371e431738f716f6473dc5981a44
shubham soni <shubham.soni1196@gmail.com>
2026-06-28 08:35:48 +0000

    add authentication syatem create and destroy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,21 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:error] = "You must be logged in to perform that action"
+      redirect_to login_path
+    end
+  end
 end
